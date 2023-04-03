2307e489ae637fad59d0a21d9eef5347a184a0b4
liuhuanxg <1985054961@qq.com>
2023-04-03 15:09:09 +0800

    update settings

--- social_distribution/settings.py
+++ social_distribution/settings.py
@@ -107,13 +107,13 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = 'Asia/shanghai'
+TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
 USE_L10N = True
 
-USE_TZ = False
+USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0.5/howto/static-files/
